c32c9ff4eba0bb266775efc279e26fcfc5284ea2
Mike251999 <miguel.sandoval0246@alumnos.udg.mx>
2025-12-07 16:32:55 -0600

    inferencias en el tipado, tipado pato y ambitos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
-let text: string = 'hola';
+let text: string = 'hola'; //inferir explicito
+let text2 = 'mundo';       //inferir no explicito
 
-// Error por tipo de valor que se quiere asignar 
-text = 1
 
-console.log(text)
+function printCoord(pt: { x: number; y: number}) {
+    console.log('el valor de la coordenada x es:' + pt.x)
+    console.log('el valor de la coordenada y es:' + pt.y)
+}
+
+printCoord({x: 2, y: 4})
+
+function print() {
+
+}
+
+let persona = { id: 1, name: 'juan', print }
+persona = { id:2, name: 'rosa', print: () => {} }
+
+let oficinista = {id: 1, name: 'Pedro', print: () => {}, capturar: () => 2}
+
+// tipado pato, para ts el oficinista paso a hacer persona y omite sus demas propiedades
+persona = oficinista
+oficinista.capturar()
+persona.print()
+
+// ambitos LET 
+
+let index = 0
+
+if (index === 0) {
+    let index = 2
+    console.log(index)
+}
+
+console.log(index)
