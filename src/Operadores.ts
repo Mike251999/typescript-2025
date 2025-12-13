0fa2b15cb4a9c19f1a88c73b7d7436cf00a5550c
Mike251999 <miguel.sandoval0246@alumnos.udg.mx>
2025-12-13 12:07:25 -0600

    Operadores

---
+++ src/Operadores.ts
@@ -0,0 +1,31 @@
+interface IdName {
+    id: number;
+    name: string;
+}
+
+interface DescrValue {
+    descr: string;
+    value: number;
+}
+
+function getDescrOrName(obj: IdName | DescrValue): string {
+    if ("id" in obj) {
+        return obj.name
+    }
+
+    return obj.descr
+}
+
+console.log(getDescrOrName({id: 1, name: "Alejandra"}))
+console.log(getDescrOrName({value: 2, descr: "test"}))
+
+// operador keyof
+
+type PropiedadNombreOId = keyof IdName
+
+function getProperty(key: PropiedadNombreOId, obj: IdName) {
+    console.log(`${key}: ${obj[key]}`)
+}
+
+getProperty("id", {id: 1, name: "Alejandra"})
+getProperty("name", {id: 1, name: "Alejandra"})
